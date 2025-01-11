d995b9ad2fd20b83c66bc63e07fefa164a08f5e4
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-11 17:39:52 +0500

    added contact schema

---
+++ src/schemas/contact.ts
@@ -0,0 +1,29 @@
+import {gql} from 'graphql-tag'
+
+export const contactSchema = gql`
+
+  # Define the Contact type (for queries)
+  type Contact{
+    id: ID!
+    name:String!
+    email:String!
+    phone:String!
+  }
+
+  type Query {
+    getContacts: [Contact]
+    getContact(_id:ID!): Contact
+  }
+
+  # Define the Contact type (for mutation)
+  input ContactInput{
+    name:String!
+    email:String!
+    phone:String!
+  }
+
+  type Mutation {
+    createContact(contact: ContactInput): String
+  }
+
+`;

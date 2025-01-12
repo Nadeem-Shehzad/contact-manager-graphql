a6272ecfb89a421af048416f9dbbcbf4d7869f7c
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-12 14:06:03 +0500

    modify to add context

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import dotenv from 'dotenv';
-
+import { Request } from 'express';
+import { IncomingMessage } from 'http';
 import connectDB from './config/dbConnection';
 import { typeDefs } from './schemas/allSchemas';
-import { resolvers } from './resolvers/allResolvers'
+import { resolvers } from './resolvers/allResolvers';
+import { context } from './middlewares/tokenValidation';
+import { MyContext } from './utils/types';
 
 dotenv.config();
 connectDB();
@@ -17,6 +20,13 @@ const server: ApolloServer = new ApolloServer({
 const startServer = async (): Promise<void> => {
     try {
         const { url } = await startStandaloneServer(server, {
+            context: async ({ req }: { req: IncomingMessage }): Promise<MyContext> => {
+                const contextData = await context({ req });
+                return {
+                    userId: contextData.userId,
+                    email: contextData.email
+                };
+            },
             listen: { port: 4000 },
         });
         console.log(`🚀 Server ready at ${url}`);

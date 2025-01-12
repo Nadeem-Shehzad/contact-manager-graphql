4fb128b39cbfac4447752d2e8595091e39b7ff72
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-12 14:05:31 +0500

    added new types for ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,17 +1,35 @@
+import { JwtPayload } from "jsonwebtoken";
+import mongoose from "mongoose";
+
 
 export interface IUser {
-    username:string;
+    username: string;
     email: string;
     password: string;
 }
 
-export interface ILogin{
-    email:string;
+
+export interface ILogin {
+    email: string;
     password: string
 }
+
 
 export interface IContact {
     name: string;
     email: string;
     phone: string;
+    user_id: mongoose.Schema.Types.ObjectId;
+}
+
+
+export interface CustomJWTPayload extends JwtPayload {
+    userId: string;
+    email: string;
+}
+
+
+export interface MyContext {
+    userId?: string;
+    email?: string;
 }

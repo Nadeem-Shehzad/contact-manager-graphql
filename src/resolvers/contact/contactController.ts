0c5927231844e483bd1f9ef262366fa136382801
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-12 14:03:32 +0500

    test all functionality

--- src/resolvers/contact/contactController.ts
+++ src/resolvers/contact/contactController.ts
@@ -1,11 +1,19 @@
+
 import Contact, { ContactDocument } from "../../models/contact";
-import { IContact } from "../../utils/types";
+import { IContact, MyContext } from "../../utils/types";
 
 
-export const mCreateContact = async (_: any, { contact }: { contact: IContact }): Promise<String> => {
+export const mCreateContact = async (_: any, { contact }: { contact: IContact }, contextValue: MyContext): Promise<String> => {
+    const { userId } = contextValue;
+
+    if (!userId) {
+        throw new Error('You must logged in!');
+    }
+
     const { name, email, phone } = contact;
 
     await Contact.create({
+        user_id: userId,
         name,
         email,
         phone
@@ -15,18 +23,73 @@ export const mCreateContact = async (_: any, { contact }: { contact: IContact })
 }
 
 
-export const qGetContacts = async (): Promise<IContact[]> => {
-    return await Contact.find({});
+export const qGetContacts = async (_: any, __: any, contextValue: MyContext): Promise<IContact[] | null> => {
+    const { userId } = contextValue;
+    if (!userId) {
+        throw new Error('You must logged in!');
+    }
+
+    return await Contact.find({ user_id: userId });
 }
 
 
 export const qGetContact = async (_: any, args: { _id: string }): Promise<IContact | null> => {
     const { _id } = args;
-    const contact = await Contact.findById({ _id });
+    const contact: ContactDocument | null = await Contact.findById({ _id });
 
     if (!contact) {
         throw new Error('Contact not found');
     }
 
     return contact;
+}
+
+
+export const mUpdateContact = async (_: any, { contactId, contact }: { contactId: String, contact: IContact }, contextValue: MyContext): Promise<String> => {
+    const { userId } = contextValue;
+    if (!userId) {
+        throw new Error('You must logged in!');
+    }
+
+    const contactExists: ContactDocument | null = await Contact.findById(contactId);
+    if (!contactExists) {
+        throw new Error('Contact not exists!');
+    }
+
+    if (contactExists.user_id.toString() !== userId) {
+        throw new Error('Access to update other data is denied!');
+    }
+
+    await Contact.findByIdAndUpdate(
+        contactId,
+        {
+            $set: {
+                ...contact
+            }
+        },
+        { new: true }
+    )
+
+    return 'Contact updated.';
+}
+
+
+export const mDeleteContact = async (_: any, { contactId }: { contactId: String }, contextValue: MyContext): Promise<String> => {
+    const { userId } = contextValue;
+    if (!userId) {
+        throw new Error('You must logged in!');
+    }
+
+    const contactExists = await Contact.findById(contactId);
+    if (!contactExists) {
+        throw new Error('Contact not exists!');
+    }
+
+    if (contactExists.user_id.toString() !== userId) {
+        throw new Error('Access to delete other data is denied!');
+    }
+
+    await Contact.findByIdAndDelete(contactId);
+
+    return 'Contact deleted.';
 }

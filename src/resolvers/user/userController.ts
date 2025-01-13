91e16d0019225bbcad71969e846082fa791a106f
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-13 22:33:25 +0500

    added pagination and redis

--- src/resolvers/user/userController.ts
+++ src/resolvers/user/userController.ts
@@ -6,6 +6,27 @@ import { IUser } from '../../utils/types';
 import { ILogin } from '../../utils/types'
 
 
+import Redis from 'ioredis';
+
+
+
+const redisClient = new Redis();
+redisClient.on('error', (err) => console.error('Redis error:', err));
+
+(async () => {
+    // Check if Redis client is already connected
+    if (redisClient.status !== 'ready') {  // 'ready' means the connection is fully established
+        try {
+            await redisClient.connect(); // Only connect if not already connected
+            console.log('Connected to Redis');
+        } catch (err) {
+            // console.error('Failed to connect to Redis:', err);
+            console.log(`connecting error...`);
+        }
+    }
+})();
+
+
 export const mSignUp = async (_: any, { userData }: { userData: IUser }): Promise<String> => {
 
     const user = await User.findOne({ email: userData.email });
@@ -23,6 +44,7 @@ export const mSignUp = async (_: any, { userData }: { userData: IUser }): Promis
 
     return 'User SignedUp!';
 }
+
 
 export const mLogin = async (_: any, { userData }: { userData: ILogin }): Promise<String> => {
     const user = await User.findOne({ email: userData.email });
@@ -48,15 +70,40 @@ export const mLogin = async (_: any, { userData }: { userData: ILogin }): Promis
     return token;
 }
 
-export const qGetUsers = async (): Promise<IUser[]> => {
-    return await User.find({})
+
+export const qGetUsers = async (_: any, { page, limit }: { page: number, limit: number })
+    : Promise<{ users: IUser[], totalUsers: number }> => {
+
+    const users = await User.find({}).skip((page - 1) * limit).limit(limit);
+    const totalUsers = users.length;
+
+    const key: string = `getUsers:${page}:${limit}`;
+
+    const cachedData = await redisClient.get(key);
+    if (cachedData) {
+        return JSON.parse(cachedData);
+    }
+
+    await redisClient.set(key, JSON.stringify({ users, totalUsers }), 'EX', 3600);
+
+    return { users, totalUsers };
 };
 
+
 export const qGetUser = async (_: any, { id }: { id: String }): Promise<IUser> => {
+
+    const key = `getUser:${id}`;
+    const cachedData = await redisClient.get(key);
+    if (cachedData) {
+        return JSON.parse(cachedData);
+    }
+
     const user = await User.findById(id);
-    if(!user){
+    if (!user) {
         throw new Error('User not found!');
     }
+
+    await redisClient.set(key, JSON.stringify(user), 'EX', 3600);
 
     return user;
 }
